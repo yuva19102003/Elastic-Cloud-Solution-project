cd4401c69029b95a28e68cbefa0dd49c67c001d0
YUVARAJ K <116803074+yuva19102003@users.noreply.github.com>
2024-02-11 21:16:12 +0530

    Delete Terraform directory

--- Terraform/modules/ec2/asg-attach/main.tf
+++
@@ -1,9 +0,0 @@
-
-
-# main.tf is for attaching the autoscaling group to the target group
-
-# Create a new ALB Target Group attachment
-resource "aws_autoscaling_attachment" "example" {
-  autoscaling_group_name = var.asg-name
-  lb_target_group_arn    = var.tg-arn
-}
--- Terraform/modules/vpc/rt/route-table/main.tf
+++
@@ -1,8 +0,0 @@
-
-
-
-# this main.tf is for creating route table for the vpc
-
-resource "aws_route_table" "route-table" {
-  vpc_id = var.route-table-vpc-id
-}
